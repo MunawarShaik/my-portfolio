3927eb18b0f6d52a21385f1ec9272b36e38079b3
munawarShaik <munawarshaik8328@gmai.com>
2022-08-23 04:15:19 +0000

    Added mobile Navbar

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,45 +1,121 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { CgMenu, CgCloseR } from "react-icons/cg";
 import "./index.css";
 import { Link } from "react-scroll";
 import Toggle from "../Toggle";
 const Navbar = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+
   return (
-    <div className="n-wrapper">
-      <div className="n-left">
-        <div className="n-name">Munawar</div>
-        <Toggle />
+    <>
+      <div className="n-wrapper">
+        <div className="n-left">
+          <div className="n-name">Munawar</div>
+          <Toggle />
+        </div>
+        <div className="n-right">
+          <div className="n-list">
+            <ul style={{ listStyleType: "none" }}>
+              <Link
+                spy={true}
+                smooth={true}
+                activeClass="activeClass"
+                to="Navbar"
+              >
+                <li>Home </li>
+              </Link>
+              <Link spy={true} smooth={true} to="Services">
+                <li>Services </li>
+              </Link>
+              <Link spy={true} smooth={true} to="Experience">
+                <li> Experience</li>
+              </Link>
+              <Link spy={true} smooth={true} to="Portfolio">
+                <li>Portfolio </li>
+              </Link>
+              <Link spy={true} smooth={true} to="Testimonial">
+                <li>Testimonial </li>
+              </Link>
+            </ul>
+          </div>
+          <Link spy={true} smooth={true} to="Contact">
+            <button className="button n-button">Contact</button>
+          </Link>
+        </div>
+        <div className="mobile-navbar-btn">
+          {openMenu ? (
+            <CgCloseR
+              size={32}
+              onClick={() => setOpenMenu(false)}
+              name="close-outline"
+              className="close-outline mobile-nav-icon "
+            />
+          ) : (
+            <CgMenu
+              size={32}
+              name="menu-outline"
+              onClick={() => setOpenMenu(true)}
+              className="mobile-nav-icon"
+            />
+          )}
+        </div>
       </div>
-      <div className="n-right">
-        <div className="n-list">
+      {openMenu ? (
+        <nav className="mob-nav-list">
           <ul style={{ listStyleType: "none" }}>
             <Link
               spy={true}
               smooth={true}
               activeClass="activeClass"
               to="Navbar"
+              onClick={() => setOpenMenu(false)}
             >
               <li>Home </li>
             </Link>
-            <Link spy={true} smooth={true} to="Services">
+            <Link
+              spy={true}
+              smooth={true}
+              to="Services"
+              onClick={() => setOpenMenu(false)}
+            >
               <li>Services </li>
             </Link>
-            <Link spy={true} smooth={true} to="Experience">
+            <Link
+              spy={true}
+              smooth={true}
+              to="Experience"
+              onClick={() => setOpenMenu(false)}
+            >
               <li> Experience</li>
             </Link>
-            <Link spy={true} smooth={true} to="Portfolio">
+            <Link
+              spy={true}
+              smooth={true}
+              to="Portfolio"
+              onClick={() => setOpenMenu(false)}
+            >
               <li>Portfolio </li>
             </Link>
-            <Link spy={true} smooth={true} to="Testimonial">
+            <Link
+              spy={true}
+              smooth={true}
+              to="Testimonial"
+              onClick={() => setOpenMenu(false)}
+            >
               <li>Testimonial </li>
             </Link>
+            <Link
+              spy={true}
+              smooth={true}
+              to="Contact"
+              onClick={() => setOpenMenu(false)}
+            >
+              <button className="button mob-nav-contact-btn">Contact</button>
+            </Link>
           </ul>
-        </div>
-        <Link spy={true} smooth={true} to="Contact">
-          <button className="button n-button">Contact</button>
-        </Link>
-      </div>
-    </div>
+        </nav>
+      ) : null}
+    </>
   );
 };
 
